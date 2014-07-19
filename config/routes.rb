32c0efe2abc0869bc15228925507c217a6022f79
Wayne Graham <wayne.graham@virginia.edu>
2014-07-19 16:32:23 -0400

    Adds static routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+  get 'about', to: 'pages#about'
+
   resources :transcriptions
+
+  root 'transcriptions#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
